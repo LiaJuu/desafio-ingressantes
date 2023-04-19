634c0d3e37ba08b25b897dfd2ef72f37b9d2459f
LiaJuu <juliadopradooliveira@gmail.com>
2023-04-19 16:13:52 -0300

    first commit

---
+++ ingressante.js
@@ -0,0 +1,39 @@
+if (document.getElementById('1').value) {
+  let select = document.querySelector('city')
+  console.log(select)
+} else if {
+  (document.getElementById('2').value) 
+    let select = document.querySelector('city2')
+} else{
+  let select = document.querySelector('city2')
+}
+
+//document.getElementById("input1").removeAttribute("hidden");
+//}else{
+//elsewhere actions
+//}
+
+//function insertValue()
+//{
+//var select = document.getElementById("select"),
+// txtVal = document.getElementById("val").value,
+// newOption = document.createElement("OPTION"),
+// newOptionVal = document.createTextNode(txtVal);
+
+//newOption.appendChild(newOptionVal);
+// select.insertBefore(newOption,select.firstChild);
+//}
+
+//<div class="input-box">
+//<label for="city">Cidade</label>
+//<select
+//  id="city"
+//  class="form-select form-select-md"
+//  aria-label=".form-select-md example"
+// >
+//  <option selected></option>
+//  <option value="1">Matem</option>
+// <option value="2">Letras</option>
+// <option value="3">Geografia</option>
+// </select>
+//</div>
